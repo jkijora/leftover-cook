00f0d5a53f1bd06b2a41d01bba9cfa7c1467e19c
BorowikJed <jedrek.kijora@gmail.com>
2020-01-05 13:22:41 +0100

    init spring

---
+++ src/main/java/eu/kijora/leftovercook/LeftoverCookApplication.java
@@ -0,0 +1,13 @@
+package eu.kijora.leftovercook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftoverCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeftoverCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kijora/leftovercook/LeftoverCookApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kijora.leftovercook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeftoverCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
